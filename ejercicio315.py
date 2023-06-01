89db6baafe7aafb989a1690dab3b021d684c4e8b
fgaete94 <f.gaete@duocuc.cl>
2023-05-31 21:18:11 -0400

    coreccion ejercicio

--- ejercicio315.py
+++ ejercicio315.py
@@ -1,10 +1,14 @@
 import numpy as np
+import random as rd
 #debemos crear una lista con 10 numeros aleatorios entre 1 al 100, luego copiar ese arreglo en otro y mostrar por pantalla el valor maximo, minimo,
 #la suma de todos los valores, el promedio y el arreglo 2 completo.
 
-arreglo=[]
-for i in range (10):
-    arreglo.append(np.random.randint(1,100))
+arreglo=np.zeros(10)
+'''for i in range (10):
+    arreglo.append(np.random.randint(1,100)) ## con esto es como si fuera una lista y nos entrega error en los print
+print(arreglo)'''
+for i in range(len(arreglo)):
+    arreglo[i]= rd.randint(1,100)
 print(arreglo)
 arreglo2=arreglo[:].copy()
 
